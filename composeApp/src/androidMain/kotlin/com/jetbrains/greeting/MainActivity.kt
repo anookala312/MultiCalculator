81d74b0ce774634859bff9d9ce222609e857e471
anookala312 <anookala@mycambrian.ca>
2024-06-12 11:23:23 -0400

    Added parameters and button to ClacEqualButton Function

--- composeApp/src/androidMain/kotlin/com/jetbrains/greeting/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/jetbrains/greeting/MainActivity.kt
@@ -4,8 +4,15 @@ import App
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import multicalculator.composeapp.generated.resources.Res
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,5 +46,12 @@ fun CalcNumericButton(){
 fun CalcOperationButton(){
 
 }
+@Composable
+fun CalcEqualsButton(display: MutableState<String>){
+    Button(onClick = { display.value = "0" }, modifier = Modifier.padding(4.dp)) {
+        Text("=")
+    }
+}
+
 
 
